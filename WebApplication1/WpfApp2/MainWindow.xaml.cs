2c4359789a49d82f17d468cd208f5fb8276d2088
ProstoGarik <bbanan570@gmail.com>
2024-06-10 22:53:19 +0300

    WPF
    Начал переход на WPF

---
+++ WebApplication1/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HubConnection hubConnection;
+        public MainWindow()
+        {
+            InitializeComponent();
+            hubConnection = new HubConnectionBuilder().WithUrl("https://localhost:7196/chat").Build();
+            hubConnection.Closed += HubConnetction_Closed;
+            Loaded += MainWindow_Loaded;
+        }
+        public async Task HubConnetction_Closed(Exception? arg)
+        {
+            listBox1.Items.Add("Соединение разорвано, переподключение...");
+            await Task.Delay(new Random().Next(0, 5) * 1000);
+            await hubConnection.StartAsync();
+        }
+        private void UpdateListBox(string message)
+        {
+            if (!listBox1.Dispatcher.CheckAccess())
+            {
+                listBox1.Dispatcher.Invoke(new Action<string>(UpdateListBox), message);
+            }
+            else
+            {
+                listBox1.Items.Add(message);
+            }
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            hubConnection.On<string>("RecieveMessage", message => { UpdateListBox(message); });
+            try
+            {
+                await hubConnection.StartAsync();
+
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add(ex.Message);
+            }
+        }
+
+        
+        
+    }
+}
